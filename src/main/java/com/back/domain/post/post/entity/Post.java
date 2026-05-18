50fdb0cb1251d8b06b8da39d3516baa771526853
jjun012 <duwns2057@naver.com>
2026-05-18 12:18:34 +0900

    feat: 댓글 구현 및 샘플 댓글 데이터 생성

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,9 +1,18 @@
 package com.back.domain.post.post.entity;
 
+import com.back.domain.post.postComment.entity.PostComment;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
@@ -12,6 +21,9 @@ public class Post extends BaseEntity {
     private String title;
     private String content;
 
+    @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE})
+    private final List<PostComment> comments = new ArrayList<>();
+
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
@@ -19,5 +31,12 @@ public class Post extends BaseEntity {
     public void modify(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(String content) {
+        PostComment postComment = new PostComment(this, content);
+        comments.add(postComment);
+
+        return postComment;
     }
 }
